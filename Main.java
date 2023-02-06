c236f27b745346ce75688cb690606a0150a81b75
Dariya Popova <dariyp26@mail.ru>
2023-02-06 16:20:07 +0300

    ДЗ к семинару № 5

---
+++ Main.java
@@ -0,0 +1,8 @@
+/* Написать калькулятор, соблюдая MVC паттерн( сложение, вычитание, умножение и деление) */
+
+public class Main {
+    public Main(){}
+    public static void main(String[] args) {
+        new View();
+    }
+}
